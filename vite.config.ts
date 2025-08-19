d614d94726da06e6190fe96c30d276eb02fdbeba
verdsoft <verdsoft@gmail.com>
2025-08-19 12:37:39 +0200

    retry

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-   base: "/royalshaven/",
+   base: "/royalshaven",
   plugins: [
     react(),
     mode === 'development' &&
